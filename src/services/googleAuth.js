4f1a3461273107ead82cee256c7cb652d9c05c0f
lulrell37 <lulrell37@gmail.com>
2026-08-11 06:12:42 -0400

    Update googleAuth.js

--- src/services/googleAuth.js
+++ src/services/googleAuth.js
@@ -1,5 +1,6 @@
 import*as Google from 'expo-auth-session/providers/google';
 import*as WebBrowser from 'expo-web-browser';
+import*as AuthSession from 'expo-auth-session';
 WebBrowser.maybeCompleteAuthSession();
 
 const ANDROID_CLIENT_ID='766739048614-4af9ehee2qnrfj6suf1khehfoun7628v.apps.googleusercontent.com';
@@ -13,9 +14,13 @@ const SCOPES=[
 ];
 
 export function useGoogleAuth(){
+  const redirectUri=AuthSession.makeRedirectUri({
+    scheme:'com.googleusercontent.apps.4af9ehee2qnrfj6suf1khehfoun7628v',
+  });
   return Google.useAuthRequest({
     androidClientId:ANDROID_CLIENT_ID,
     webClientId:WEB_CLIENT_ID,
     scopes:SCOPES,
+    redirectUri,
   });
 }
